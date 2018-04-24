58444301bbafc351a3552f472416c4d1a4df9e9f
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-24 00:54:56 -0500

    implement post todo endpoint

--- server/server.js
+++ server/server.js
@@ -1,67 +1,25 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-      type: String,
-      required: true,
-      minLength: 1,
-      trim: true
-    },
-    completed: {
-      type: Boolean,
-      default: false
-    },
-    completedAt: {
-      type: Number,
-      default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.status(200).send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+    //console.log('Unable to save todo ', e);
+  })
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook Dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('unable to save Todo.', err);
-// });
-
-
-// var cozTodo = new Todo({
-//   text: 'Walk at the park',
-//   completed: false,
-//   completedAt: 123654
-// });
-
-// cozTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('unable to save Todo.', err);
-// });
-
-// var otherTodo = new Todo({});
-
-// otherTodo.save().then((doc) => {
-//   console.log(JSON.stringify(doc, undefined, 2));
-// });
-
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minLength: 1,
-    trim: true
-  }
-});
-
-var cozUser = new User({
-  email: 'jcoz1968@gmail.com'
-});
-
-cozUser.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });

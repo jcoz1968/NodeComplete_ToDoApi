4084cb890f613a555fe3c035bd29f81137d82bef
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-26 16:35:33 -0500

    heroku config

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {

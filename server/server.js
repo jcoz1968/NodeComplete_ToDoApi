1e2f9fbec3b78d5acf2364bfc464ae3275efa8b2
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-25 21:04:26 -0500

    add get todos api endpoint

--- server/server.js
+++ server/server.js
@@ -16,8 +16,17 @@ app.post('/todos', (req, res) => {
     res.status(200).send(doc);
   }, (e) => {
     res.status(400).send(e);
-    //console.log('Unable to save todo ', e);
   })
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    });
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.listen(3000, () => {
